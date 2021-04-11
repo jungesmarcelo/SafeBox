9996a1ba8fa81f90e23c59c05dda2403b628cc2a
Marcelo Junges <marcelo.junges@byne.com.br>
2021-04-10 22:39:39 -0300

    integrate with limit switch sensor

--- main.cpp
+++ main.cpp
@@ -1,28 +1,31 @@
-/*  ___   ___  ___  _   _  ___   ___   ____ ___  ____  
- * / _ \ /___)/ _ \| | | |/ _ \ / _ \ / ___) _ \|    \ 
- *| |_| |___ | |_| | |_| | |_| | |_| ( (__| |_| | | | |
- * \___/(___/ \___/ \__  |\___/ \___(_)____)___/|_|_|_|
- *                  (____/ 
+/* 
+ * PROJETO INTEGRADOR 5 - IFSC
+ *
  * 
- * Tutorial URL 
- * CopyRight John Yu
+ *   Author: Marcelo Junges
+ *
+ *  
  */
+
+
 #include <Arduino.h>
 #include <WiFi.h>
 #include <PubSubClient.h>
 //#include <Servo.h>
 
 
-// Update these with values suitable for your network.
+// Network setings
 const char* ssid = "Suzana";
 const char* password = "20461203";
 const char* mqtt_server = "161.35.227.231";
 
 
-// Pinos conectados ao Step e Dir do Easydriver
-int pino_passo = 17;
-int pino_direcao = 18;
-int direcao = 1;
+// Default pins
+int pino_passo = 17;        // Step pin
+int pino_direcao = 18;      // Direction pin
+const int pino_chave1 = 23;   // End of course 1 pin 
+const int pino_chave2 = 24;   // End of course 1 pin
+int direcao = 1;            // Direction variable inicialize 
 
 
 //Servo myservo;  // create servo object to control a servo
@@ -32,7 +35,7 @@ PubSubClient client(espClient);
 
 void setup_wifi() {
    delay(100);
-  // We start by connecting to a WiFi network
+  // connecting to a WiFi network
     Serial.print("Connecting to ");
     Serial.println(ssid);
     WiFi.begin(ssid, password);
@@ -52,45 +55,46 @@ void setup_wifi() {
 
 void callback(char* topic, byte* payload, unsigned int length) 
 {
+
   Serial.println("Command from MQTT broker is :   ");
   //Serial.println((char*)payload);
   
   int location=String((char*)payload).toInt();
   Serial.println(location);
+  
+
   //if((location>194)||(location<0))
     //return;
-    //if((int)payload[i]>194||(int)payload[i]<0)
+    //if((int)payload[i]>194||(int)payload[i]<0) 
 
-  if(location>0)
-    {
-      // Define a direcao de rotacao
-      direcao = 1;
-      digitalWrite(pino_direcao, direcao);
-      for (int p=0 ; p < location; p++)
-      {
-        Serial.println(p);
-        digitalWrite(pino_passo, 1);
-        delay(1);
-        digitalWrite(pino_passo, 0);
-        delay(1);
-      }
+  if(location>0) {
+    // Define a direcao de rotacao
+    direcao = 1;
+    digitalWrite(pino_direcao, direcao);
     
-    }
-  if(location<0)
-  {
-  // Inverte a direcao de rotacao
-  direcao = 0;
-  location = -location;
-  Serial.println(location);
-  digitalWrite(pino_direcao, direcao);
-    for (int p=0 ; p < location; p++)
-    {
-      Serial.println(p);
+    for (int p=0 ; p < location; p++) {
+      //Serial.println(p);
       digitalWrite(pino_passo, 1);
       delay(1);
       digitalWrite(pino_passo, 0);
       delay(1);
-     }
+    }
+  }
+
+  else if(location<0) {
+    // Inverte a direcao de rotacao
+    direcao = 0;
+    location = -location;
+    //Serial.println(location);
+    digitalWrite(pino_direcao, direcao);
+
+    for (int p=0 ; p < location; p++) {
+      //Serial.println(p);
+      digitalWrite(pino_passo, 1);
+      delay(1);
+      digitalWrite(pino_passo, 0);
+      delay(1);
+    }
   }
 
   location = 0;  
@@ -132,12 +136,28 @@ void setup() {
   // Define os pinos como saida
   pinMode(pino_passo, OUTPUT);
   pinMode(pino_direcao, OUTPUT);
+  pinMode(pino_chave1, INPUT_PULLUP);
+  pinMode(pino_chave2, INPUT_PULLUP);
 
   Serial.begin(115200);
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
   //myservo.attach(D1);  // attaches the servo on pin D1 to the servo object
+  
+  if(digitalRead(pino_chave1) == HIGH)
+  { 
+    while(digitalRead(pino_chave1) == HIGH)
+      {
+      direcao = 0;
+      digitalWrite(pino_direcao, direcao);
+      digitalWrite(pino_passo, 1);
+      delay(1);
+      digitalWrite(pino_passo, 0);
+      delay(1);
+      }
+  }
+
 }
 
 void loop() {
